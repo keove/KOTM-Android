0eae466f4d0db1d48d5d5dc777c00046c9d8b68a
Çağrı Özdeş <cagri.ozdes@keove.com>
2019-10-29 00:54:43 +0300

    - update for kotm2

--- KOTMLibrary/src/main/java/com/keove/mylibrary/KOTM.java
+++ KOTMLibrary/src/main/java/com/keove/mylibrary/KOTM.java
@@ -63,15 +63,15 @@ public class KOTM extends Activity{
 
                     if(Button.class.isAssignableFrom(field.getType())) {
                         Button btn = (Button)field.get(object);
-                        btn.setText(elementValue(context,KotmElement.text,tag));
+                        btn.setText(elementValue(context,KotmElement.value,tag));
                     }
                     else if(EditText.class.isAssignableFrom(field.getType())) {
                         EditText et = (EditText)field.get(object);
-                        et.setHint(elementValue(context,KotmElement.placeholder,tag));
+                        et.setHint(elementValue(context,KotmElement.value,tag));
                     }
                     else if(TextView.class.isAssignableFrom(field.getType())) {
                         TextView tv = (TextView)field.get(object);
-                        tv.setText(elementValue(context,KotmElement.text,tag));
+                        tv.setText(elementValue(context,KotmElement.value,tag));
                     }
                     else if(String.class.isAssignableFrom(field.getType())) {
                         field.set(object,elementValue(context,KotmElement.value,tag));
@@ -155,189 +155,5 @@ public class KOTM extends Activity{
 
 
 
-    // region DEPRECATED METHODS
-
-    @Deprecated
-    public static String getTranslation(Context context, String text,String languageCode) {
-        String json = valueByContext(context.getApplicationContext(), "translations", text);
-
-        try {
-            JSONObject mainJsonObject = new JSONObject(json);
-            JSONObject textJsonObject = mainJsonObject.getJSONObject(text);
-            JSONObject valueObject= textJsonObject.getJSONObject("value");
-            String t = valueObject.getString(languageCode);
-
-            if(t!=null && t.length() > 0) {
-                return t;
-            }
-            else return text;
-
-        }
-        catch (Exception e) {
-            return text;
-        }
-    }
-
-    @Deprecated
-    public static void translate(Object object, String languageCode, Context context) {
-
-
-        ArrayList<String> fieldNames= new ArrayList<>();
-
-        Field[] fields= object.getClass().getDeclaredFields();
-        ArrayList<String> annotationList= new ArrayList<>();
-
-        for(Field f: fields) {
-            fieldNames.add(f.getType().getSimpleName());
-            try {
-                KOTMTag notation = f.getAnnotation(KOTMTag.class);
-                if (notation != null){
-                    annotationList.add(notation.value());
-                } else {
-                    annotationList.add("");
-                }
-            }
-            catch (Exception ex) {
-                ex.getStackTrace();
-            }
-        }
-
-        for (int i=0; i<=fieldNames.size()-1; i++) {
-
-            Field field = fields[i];
-            Class k = field.getType().getClass();
-
-            try {
-
-                if(k.isInstance(ArrayList.class)) {
-
-                    ArrayList<String> stringArrayList= (ArrayList<String>) fields[i].get(object);
-                    ArrayList<String> textArray = getTextArrays(context,annotationList.get(i),languageCode,String.class);
-
-                    for (int a=0; a<=textArray.size()-1; a++) {
-                        stringArrayList.add(textArray.get(a));
-                    }
-
-                } else if (k.isInstance(String.class) && !k.isInstance(ArrayList.class)) {
-
-                    String textForStrings= getTextForStrings(context,annotationList.get(i),languageCode);
-                    field.set(object,textForStrings);
-
-                } else if (k.isInstance(Button.class)) {
-
-                    String textForTextsAndButtons = getTextForTextsAndButtons(context, annotationList.get(i), languageCode);
-                    Button fieldButton = (Button) fields[i].get(object);
-                    fieldButton.setText(textForTextsAndButtons);
-
-                } else if (k.isInstance(EditText.class)) {
-
-                    EditText fieldEditText = (EditText) fields[i].get(object);
-                    String textForEditText= getTextForEditText(context,annotationList.get(i),languageCode);
-                    fieldEditText.setHint(textForEditText);
-
-                } else if (k.isInstance(TextView.class)) {
-
-                    TextView fieldTextView = (TextView) fields[i].get(object);
-                    String textForTextsAndButtons= getTextForTextsAndButtons(context,annotationList.get(i),languageCode);
-                    fieldTextView.setText(textForTextsAndButtons);
-
-                }
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    @Deprecated
-    private static <T extends Object> ArrayList<T> getTextArrays(Context context, String annotation,String languageCode,Class<T> klass) {
-
-        String json = valueByContext(context.getApplicationContext(), "translations", annotation);
-        ArrayList<T> list = new ArrayList<>();
-
-        try {
-
-            JSONObject mainJsonObject = new JSONObject(json);
-            JSONObject translationNodeObject= mainJsonObject.getJSONObject(annotation);
-            JSONObject valueObject= translationNodeObject.getJSONObject("value");
-            JSONArray valueObjectJSONArray= valueObject.getJSONArray(languageCode);
-
-            for (int i = 0; i < valueObjectJSONArray.length(); i++) {
-                String listValues= valueObjectJSONArray.getString(i);
-                T t = klass.cast(listValues);
-                list.add(t);
-            }
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return list;
-    }
-
-    @Deprecated
-    private static String getTextForTextsAndButtons(Context context, String annotation, String languageCode) {
-        String json = valueByContext(context.getApplicationContext(), "translations", annotation);
-
-        try {
-            JSONObject mainJsonObject = new JSONObject(json);
-            JSONObject translationNodeObject= mainJsonObject.getJSONObject(annotation);
-            JSONObject textObject= translationNodeObject.getJSONObject("text");
-            String t= textObject.getString(languageCode);
-
-            if(t!=null && t.length() > 0) {
-                return t;
-            } else {
-                return "";
-            }
-        }
-        catch (Exception e) {
-            return annotation;
-        }
-    }
-    
-    @Deprecated
-    private static String getTextForStrings(Context context, String annotation,String languageCode) {
-        String json = valueByContext(context.getApplicationContext(), "translations", annotation);
-
-        try {
-            JSONObject mainJsonObject = new JSONObject(json);
-            JSONObject translationNodeObject= mainJsonObject.getJSONObject(annotation);
-            JSONObject valueObject= translationNodeObject.getJSONObject("value");
-            String t= valueObject.getString(languageCode);
-
-            if(t!=null && t.length() > 0) {
-                return t;
-            } else {
-                return "";
-            }
-        }
-        catch (Exception e) {
-            return annotation;
-        }
-    }
-
-    @Deprecated
-    private static String getTextForEditText(Context context, String annotation,String languageCode) {
-        String json = valueByContext(context.getApplicationContext(), "translations", annotation);
-
-        try {
-            JSONObject mainJsonObject = new JSONObject(json);
-            JSONObject translationNodeObject= mainJsonObject.getJSONObject(annotation);
-            JSONObject placeholderObject= translationNodeObject.getJSONObject("placeholder");
-            String t= placeholderObject.getString(languageCode);
-
-            if(t!=null && t.length() > 0) {
-                return t;
-            } else {
-                return "";
-            }
-        }
-        catch (Exception e) {
-            return annotation;
-        }
-    }
-
-    // endregion
 }
 
